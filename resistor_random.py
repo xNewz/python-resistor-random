c890fd735f7aef94d1cabc54ef34864265c8373b
xNewz <pargornruasijan@gmail.com>
2021-12-02 00:26:29 +0700

    Add random color 4 bands

--- resistor_random.py
+++ resistor_random.py
@@ -7,3 +7,8 @@ color1 = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'purple'
 color2 = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'gray', 'white']
 color3 = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'gold', 'silver']
 color4 = ['gold', 'silver']
+
+ran_c1 = random.choice(color1)
+ran_c2 = random.choice(color2)
+ran_c3 = random.choice(color3)
+ran_c4 = random.choice(color4)
