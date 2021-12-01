89ce35549d12abe089351be6778bb28b6c09f193
xNewz <pargornruasijan@gmail.com>
2021-12-02 00:26:06 +0700

    Add var 4 bands

--- resistor_random.py
+++ resistor_random.py
@@ -2,3 +2,8 @@ import random
 import time
 
 time_delay = int(input('enter time delay: '))
+
+color1 = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'gray', 'white']
+color2 = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'gray', 'white']
+color3 = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'gold', 'silver']
+color4 = ['gold', 'silver']
